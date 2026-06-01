9ac8ccea79f5464c9e372c9229c273e885623e26
md-sadidtahsin <sadidtahsin@hotmail.com>
2026-06-01 15:23:38 +0600

    python test14

--- python-service/app.py
+++ python-service/app.py
@@ -325,4 +325,4 @@ def get_stats():
 if __name__ == "__main__":
     init_db()
     init_redis()
-    app.run(host="0.0.0.0", port=5000,  debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)
